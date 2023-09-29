a00fb4b8506ad5c3577c6b6970a3fe0bcd7d9131
Bonesaw <langgeorg61@gmail.com>
2023-09-29 15:51:36 +0200

    Enabled firefox with basic profile

---
+++ firefox/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+
+    programs.firefox = {
+        enable = true;
+        profiles = {
+            bonesaw = {
+                id = 0;
+                name = "bonesaw";
+                default = true;
+                search = {
+                    default = "Google";
+                    force = true;
+                    order = [
+                        "Google"
+                        "DuckDuckGo"
+                    ];
+                };
+
+                # All firefox settings from about:config can be enabled here
+                # Home-manager will convert all JSON-compatible values into strings
+                setting = {
+                    "browser.startup.homepage" = "https://google.com";
+                    "browser.search.region" = "DE";
+                };
+
+                bookmarks = [
+                    {
+                        name = "Youtube";
+                        url = "https://www.youtube.com";
+                    }
+                    {
+                        name = "Twitch";
+                        url = "https://www.twitch.tv";
+                    }
+                    {
+                        name = "Reddit";
+                        url = "https://www.reddit.com";
+                    }
+                    {
+                        name = "Github";
+                        url = "https://www.github.com";
+                    }
+                ];
+
+                #TODO: Install NUR for extensions
+                extensions = [
+
+                ];
+            };
+        };
+    };
+}
