eb18313f7d177622491d508c028a211e48760dd7
Bonesaw <langgeorg61@gmail.com>
2023-09-30 00:27:25 +0200

    enabled lazyvim

--- nvim/default.nix
+++ nvim/default.nix
@@ -3,10 +3,6 @@
   config,
   ...
 }: {
-  imports = [
-    ./lunarvim
-  ];
-
   programs.neovim = {
     enable = true;
 
@@ -23,8 +19,9 @@
 
   # config files 
   home.file.".config/nvim" = {
-      source = ./lunarvim/config;
+      source = ./lazyvim;
       recursive = true;
+      executable = true;
   };
 
   home = {
@@ -36,7 +33,33 @@
       llvmPackages.clang-unwrapped # c/c++ tools with clang-tools such as clangd
       cmake
       cmake-language-server
+      gnumake
 
+      #-- nix
+      nil
+      rnix-lsp
+      # nixd
+      statix # Lints and suggestions for the nix programming language
+      deadnix # Find and remove unused code in .nix source files
+      alejandra # Nix Code Formatter
+
+      # lua
+      stylua
+      lua-language-server
+
+      # python
+      python3
+      
+      # javascript
+      nodejs
+
+      # rust
+      cargo
+
+      # misc
+      tree-sitter
+
+      # telescope
       fd
       ripgrep
     ]
