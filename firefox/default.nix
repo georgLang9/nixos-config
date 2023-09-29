9046d565b104f5e34e69f893e361827b0fea44a1
Bonesaw <langgeorg61@gmail.com>
2023-09-29 16:41:44 +0200

    Added extensions

--- firefox/default.nix
+++ firefox/default.nix
@@ -46,7 +46,43 @@
                 ];
 
                 extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+                    #=================== Utility ==============================
                     bitwarden
+                    ublock-origin
+                    vimium
+                    no-pdf-download
+
+                    #=================== Social Media =========================
+                    # Twitch
+                    betterttv
+
+                    # Youtube
+                    return-youtube-dislikes
+                    sponsorblock
+                    youtube-shorts-block
+                    enhancer-for-youtube
+
+                    # Reddit
+                    old-reddit-redirect
+                    reddit-enhancement-suite
+
+                    # Twitter
+                    nitter-redirect
+                    
+                    # Steam
+                    protondb-for-steam
+                    augmented-steam
+
+                    #=================== Privacy ==============================
+                    # Cookies
+                    cookie-autodelete
+                    consent-o-matic
+                    i-dont-care-about-cookies
+
+                    # Privacy
+                    decentraleyes
+                    privacy-badger
+
                 ];
             };
         };
