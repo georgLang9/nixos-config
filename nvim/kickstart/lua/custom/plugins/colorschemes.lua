d6d621a03042b428b63cd822cc69924c5a3e7dc3
Bonesaw <langgeorg61@gmail.com>
2023-09-30 12:37:58 +0200

    Split up kickstart config

---
+++ nvim/kickstart/lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'onedark'
+    -- end,
+  },
+
+  -- Tokyo Night theme
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+}
---
+++ nvim/kickstart/lua/custom/plugins/smallPlugins.lua
@@ -0,0 +1,19 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    main = 'ibl',
+    opts = {},
+  },
+}
