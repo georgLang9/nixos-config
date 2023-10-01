556fb325a674fc0613d3c4d12d785dab0172f11a
Bonesaw <langgeorg61@gmail.com>
2023-10-01 17:58:17 +0200

    Excluded gnome apps

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,27 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  environment.gnome.excludePackages = with pkgs.gnome ; [
+    baobab        # disk usage analyzer
+    cheese        # photo booth
+    eog           # image viewer
+    epiphany      # web browser
+    gedit         # text editor
+    simple-scan   # document scanner
+    totem         # video player
+    yelp          # help viewer
+    evince        # document viewer
+    file-roller   # archive manager
+    geary         # email client
+    seahorse      # password manager
+
+    # these should be self explanatory
+    gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
+    gnome-logs gnome-maps gnome-music gnome-photos gnome-weather  
+
+    # gnome-system-monitor gnome-disk-utility pkgs.gnome-connections gnome-screenshot
+  ];
+
   # Configure keymap in X11
   services.xserver = {
     layout = "de";
