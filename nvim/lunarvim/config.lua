fec12a97361692826b7b52b438430ac445916b0e
Bonesaw <langgeorg61@gmail.com>
2023-10-02 19:57:12 +0200

    added keymap for closing buffer

--- nvim/lunarvim/config.lua
+++ nvim/lunarvim/config.lua
@@ -10,3 +10,6 @@ lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 -- Switch between buffers
 lvim.keys.normal_mode["<S-h>"] = ":bprevious<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+
+-- Close buffer
+lvim.keys.normal_mode["<S-l>"] = ":BufferKill<CR>"
